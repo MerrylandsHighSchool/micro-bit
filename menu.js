b0fce29c13045e9049c4cd5737bf8c5eeb0e354c
Ben Jones <benjamin.jones21@det.nsw.edu.au>
2020-02-03 13:00:23 +1100

    added JS driven menu

---
+++ menu.js
@@ -0,0 +1,4 @@
+// Write each link as a new line and place back slashes in front of any grammar
+document.write("<a class='dropdown-item waves-effect waves-light' href='tutorial1.html'>Tutorial 1</a>");
+document.write("<a class='dropdown-item waves-effect waves-light' href='tutorial2.html'>Tutorial 2</a>");
+document.write("<a class='dropdown-item waves-effect waves-light' href='tutorial3.html'>Tutorial 3</a>");
