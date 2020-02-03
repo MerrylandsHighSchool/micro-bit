2161eb48b5770300e7a745e0a6c6e4c2d0e16b72
Ben Jones <benjamin.jones21@det.nsw.edu.au>
2020-02-04 09:33:49 +1100

    prepare for new content

--- menu.js
+++ menu.js
@@ -2,3 +2,4 @@
 document.write("<a class='dropdown-item waves-effect waves-light' href='tutorial1.html'>Tutorial 1</a>");
 document.write("<a class='dropdown-item waves-effect waves-light' href='tutorial2.html'>Tutorial 2</a>");
 document.write("<a class='dropdown-item waves-effect waves-light' href='tutorial3.html'>Tutorial 3</a>");
+document.write("<a class='dropdown-item waves-effect waves-light' href='tutorial4.html'>Tutorial 4</a>");
